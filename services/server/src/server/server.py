86d575e752862c2f595713185abfb18af9cad1c0
emigomez28 <egomez@fi.uba.ar>
2026-08-30 16:23:21 -0300

    feat: implemento el handleo de los mensajes que envia el cliente para las apuestas

--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,39 +1,36 @@
 import socket
 import logger
-import safe_socket
+import protocol
+from lottery import Lottery
 
 _ECHO_SERVER_MESSAGE_SIZE = 1024
 
 
 class Server:
-    def __init__(self, server_host: str, server_port: int) -> None:
+    def __init__(self, server_host: str, server_port: int, storage_path: str) -> None:
         self.server_host = server_host
         self.server_port = server_port
+        self.lottery = Lottery(storage_path)
 
     def _handle_client(self, client_socket):
         action = "handle-client"
-        message_amount = 0
+
         try:
-            logger.info(action, logger.LogResult.in_progress)
-            while True:
-                client_message = safe_socket.recv_all(
-                    client_socket, _ECHO_SERVER_MESSAGE_SIZE
-                )
-                if not client_message:
-                    logger.info(
-                        action,
-                        logger.LogResult.success,
-                        "messages-amount",
-                        message_amount,
-                    )
-                    return
-                message_amount += 1
-                safe_socket.send_all(client_socket, client_message)
+            agency_id = self._recv_start_transmission(client_socket)
+            bets_amount = self._recv_bets(client_socket, agency_id)
+            self._send_winners(client_socket, agency_id)
         except Exception as e:
-            logger.error(
-                action, logger.LogResult.fail, "messages-amount", message_amount
-            )
-            raise e
+            logger.error(action, logger.LogResult.fail, "err", e)
+            return
+
+        logger.info(
+            action,
+            logger.LogResult.success,
+            "agency-id",
+            agency_id,
+            "bets-amount",
+            bets_amount,
+        )
 
     def run(self):
         action = "accept-connection"
@@ -49,4 +46,69 @@ class Server:
                     raise e
                 logger.info(action, logger.LogResult.success)
 
-                self._handle_client(client_socket)
+                with client_socket:
+                    self._handle_client(client_socket)
+
+    def _recv_expecting(self, client_socket, expected: int) -> protocol.Message:
+        message = protocol.recv_message(client_socket)
+
+        if message.type != expected:
+            expected_name = protocol.MessageType.get_name_from_code(expected)
+            received_name = protocol.MessageType.get_name_from_code(message.type)
+            raise RuntimeError(f"expected {expected_name}, got {received_name}")
+
+        return message
+
+    def _recv_start_transmission(self, client_socket) -> int:
+        message = self._recv_expecting(
+            client_socket, protocol.MessageType.START_TRANSMISSION
+        )
+
+        agency_id = protocol.decode_agency_id(message.payload)
+        protocol.send_message(client_socket, protocol.Message(protocol.MessageType.OK))
+        return agency_id
+
+    def _recv_bets(self, client_socket, agency_id) -> int:
+        bets_amount = 0
+
+        while True:
+            message = protocol.recv_message(client_socket)
+
+            if message.type == protocol.MessageType.END_TRANSMISSION:
+                break
+
+            if message.type != protocol.MessageType.BET:
+                received_name = protocol.MessageType.get_name_from_code(message.type)
+                raise RuntimeError(
+                    f"expected BET or END_TRANSMISSION, got {received_name}"
+                )
+
+            bet = protocol.decode_bet(message.payload, agency_id)
+            self.lottery.store_bets([bet])
+
+            bets_amount += 1
+
+            protocol.send_message(
+                client_socket, protocol.Message(protocol.MessageType.OK)
+            )
+
+        return bets_amount
+
+    def _send_winners(self, client_socket, agency_id: int) -> None:
+        winners = self._get_winners(agency_id)
+
+        protocol.send_message(
+            client_socket,
+            protocol.Message(
+                protocol.MessageType.WINNERS, protocol.encode_winners(winners)
+            ),
+        )
+
+    def _get_winners(self, agency_id):
+        winners = []
+
+        for bet in self.lottery.load_bets():
+            if bet.agency_id == agency_id and self.lottery.has_won(bet):
+                winners.append(bet)
+
+        return winners
